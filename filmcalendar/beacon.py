126273e52cc3d56a0d5620d6fcc8215c5fa3a3c4
BryantD <durrell@innocence.com>
2022-08-16 18:49:00 -0700

    Calendar event changes: UID add
    Added a UID field, which is important for icalendar. Format is a bit hacky:
    
    <timestamp>-<event #>@<hostname>
    
    Event # is just a running count; I don't bother adding this till I write out the file. This is not actually in spec, since UID is supposed to stay the same for any given calendar event. I'll start thinking about fixing this if I ever start tracking events in a database, which would have some advantages but not enough to be a priority for 1.0.
    
    Also snuck in a hardcoded skip for "Rent The Beacon" events, which really are not in scope for upcoming movies.

--- filmcalendar/beacon.py
+++ filmcalendar/beacon.py
@@ -29,22 +29,24 @@ class FilmCalendarBeacon(filmcalendar.FilmCalendar):
                     film_title = film.find("section", itemprop="name").get_text().title()
                 except TypeError as error:
                     raise ValueError("Couldn't find film name") from error
-                try:
-                    timezone = pytz.timezone("US/Pacific")
-                    film_date = timezone.localize(datetime.fromisoformat(film.find("section", itemprop="startDate")["content"]))
-                except TypeError as error:
-                        raise ValueError("Couldn't find film start time") from error     
-                try:
-                    film_url = film.find("a")["href"]
-                except TypeError as error:
-                    film_url = None
-                try:
-                    film_location = film.find("span", itemprop="streetAddress").get_text() + ", "
-                    film_location = film_location + film.find("span", itemprop="addressLocality").get_text() + ", "
-                    film_location = film_location + film.find("span", itemprop="addressRegion").get_text() + " "
-                    film_location = film_location + film.find("span", itemprop="postalCode").get_text()
-                except TypeError as error:
-                    film_location = None
-                film_duration = 120 * 60
+                if film_title != "Rent The Beacon":
+                    # Hardcoding a skip for rental slots
+                    try:
+                        timezone = pytz.timezone("US/Pacific")
+                        film_date = timezone.localize(datetime.fromisoformat(film.find("section", itemprop="startDate")["content"]))
+                    except TypeError as error:
+                            raise ValueError("Couldn't find film start time") from error     
+                    try:
+                        film_url = film.find("a")["href"]
+                    except TypeError as error:
+                        film_url = None
+                    try:
+                        film_location = film.find("span", itemprop="streetAddress").get_text() + ", "
+                        film_location = film_location + film.find("span", itemprop="addressLocality").get_text() + ", "
+                        film_location = film_location + film.find("span", itemprop="addressRegion").get_text() + " "
+                        film_location = film_location + film.find("span", itemprop="postalCode").get_text()
+                    except TypeError as error:
+                        film_location = None
+                    film_duration = 120 * 60
 
-                self.add_event(summary=film_title, dtstart=film_date, duration=film_duration, url=film_url, location=film_location)
+                    self.add_event(summary=film_title, dtstart=film_date, duration=film_duration, url=film_url, location=film_location)
--- filmcalendar/filmcalendar.py
+++ filmcalendar/filmcalendar.py
@@ -12,6 +12,7 @@ class FilmCalendar:
         self.cal.add("x-wr-calname", "Seattle Arthouse Film Calendar")
         self.cal.add("x-wr-timezone", "US/Pacific")
         self.theater = ""
+        self.uid_base = "seattle-movies.innocence.com"
 
     def __str__(self):
         cal_string = self.cal.to_ical()
@@ -21,21 +22,35 @@ class FilmCalendar:
         for event in calendar.cal.walk(name="vevent"):
             self.cal.add_component(event)
 
-    def add_event(self, summary=None, dtstart=None, url=None, duration=0, location=None):
+    def add_event(self, summary=None, dtstart=None, url=None, duration=120*60, location=None):
         event = Event()
+        
+        # Required components
         event.add("summary", summary)
         event.add("dtstart", vDatetime(dtstart))
-        if duration:
-            event.add("duration", timedelta(seconds=duration))
+        event.add("duration", timedelta(seconds=duration))
+        event.add("description", f"{self.theater}\n{url}")
+
+        # Optional components
         if url:
             event.add("url", url)
         if location:
             event.add("location", location)
-        event.add("description", f"{self.theater}\n{url}")
+        
+        # Auto-generated components
+        event.add("dtstamp", vDatetime(datetime.now()))
+        
         self.cal.add_component(event)
 
     def write(self, filename="film_calendar.ics"):
         self.cal.add("last-modified", vDatetime(datetime.now()))
+        event_count = 0
+        for event in self.cal.walk(name="vevent"):
+            uid_timestamp = datetime.now().strftime("%Y%m%dT%H%M%S")
+            event_count = event_count + 1
+            uid = f"{uid_timestamp}-{event_count}@{self.uid_base}"
+            event.add("uid", uid)
+
         f = open(filename, "wb")
         f.write(self.cal.to_ical())
         f.close()
