9ed1db78b0d85b7b072836cc552f69f8270e6e10
BryantD <durrell@innocence.com>
2022-08-18 18:13:55 -0700

    SIFF scraper complete
    SIFF uses the same event manager as Central Cinema behind the scenes, which saved me some time. You only get one day at a time so scraping takes a bit longer, but so it goes.
    
    Fixes #1 and we are thus code-complete.

--- filmcalendar/siff.py
+++ filmcalendar/siff.py
@@ -4,32 +4,26 @@ from icalendar import Calendar, Event
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-import pytz
+import json
+import html
 
 
 class FilmCalendarSIFF(filmcalendar.FilmCalendar):
     def __init__(self):
         super().__init__()
         self.theater = "SIFF"
-        self.base_url = "https://www.siff.net/"
+        self.base_url = "https://www.siff.net"
         self.addresses = {
-            "Egyptian": "805 E. Pine St, Seattle, WA, 98122",
-            "Film Center": "305 Harrison St, Seattle, WA 98109",
-            "Uptown": "511 Queen Anne Ave N, Seattle, WA 98109",
+            "SIFF Cinema Egyptian": "805 E. Pine St, Seattle, WA, 98122",
+            "SIFF Film Center": "305 Harrison St, Seattle, WA 98109",
+            "SIFF Cinema Uptown": "511 Queen Anne Ave N, Seattle, WA 98109",
         }
 
     def __str__(self):
         return super().__str__()
 
-    def _parse_duration(self, duration_raw):
-        # Return duration in seconds -- format is "120 min."
-        if duration_raw[-4:] == "min.":
-            return int(duration_raw[:-5]) * 60
-        else:
-            return 120 * 60
-
     def _fetch_film_page(self, date):
-        req_payload = {"type": "view", "list": start_date.strftime("%Y-%m-%d")}
+        req_payload = {"view": "list", "date": date.strftime("%Y-%m-%d")}
         try:
             req = requests.get(
                 "https://www.siff.net/calendar",
@@ -42,23 +36,34 @@ class FilmCalendarSIFF(filmcalendar.FilmCalendar):
 
         soup = BeautifulSoup(req.text, "html.parser")
 
-        listings = soup.find("div", class_="listing")
-        for film in listings.find("div", class_="item"):
-            self.add_event(
-                summary=film_title,
-                dtstart=film_date,
-                duration=film_duration,
-                url=film_url,
-                location=film_location,
-            )
+        if listings := soup.find("div", class_="listing thumbs"):
+            for film in listings.find_all("div", class_="item"):
+                film_anchor = film.find("div", class_="small-copy").find("h3").find("a")
+                film_title = film_anchor.get_text().strip()
+                film_url = f"{self.base_url}{film_anchor['href']}"
+        
+                film_times = film.find("div", class_="times")
+                film_theater = film_times.find("span", class_="dark-gray-text").get_text().strip()
+                film_location = f"{self.theater}: {self.addresses[film_theater]}"
+                for screening in film.find_all("a", class_="elevent"):
+                    event_json = json.loads(html.unescape(screening["data-screening"]))
+                    film_duration = event_json["LengthInMinutes"] * 60
+                    film_date = datetime.fromtimestamp(int(event_json["Showtime"][6:-2])/1000)
+        
+                    self.add_event(
+                        summary=film_title,
+                        dtstart=film_date,
+                        duration=film_duration,
+                        url=film_url,
+                        location=film_location,
+                    )
 
     def fetch_films(self):
-        start_date = datetime.now() - timedelta(days=datetime.now().isoweekday() - 1)
-        end_date = start_date + timedelta(weeks=5)
+        start_date = datetime.now()
+        end_date = start_date + timedelta(weeks=5) 
 
         # Decision: we'll loop 5 weeks into the future; that looks like about how far
         # out SIFF's scheduling goes
-
         while start_date < end_date:
             self._fetch_film_page(start_date)
             start_date = start_date + timedelta(days=1)
