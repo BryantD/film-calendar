36cb60936a155c5d3829d129860a4ba3af6eab28
BryantD <durrell@innocence.com>
2022-08-27 17:27:43 -0700

    Added directory CLI argument, removed output CLI argument
    Now that I'm generating multiple ICS files, one output argument doesn't make sense. I'm sure there's a world where you want to be able to control the location of each theater's calendar file but it's not my world so I'm not implementing that one.

--- make_film_calendar.py
+++ make_film_calendar.py
@@ -2,6 +2,7 @@
 
 import argparse
 import importlib
+from pathlib import Path
 
 import filmcalendar.filmcalendar
 
@@ -25,25 +26,32 @@ def main():
         default=theaters.keys(),
     )
     parser.add_argument(
-        "output", nargs="?", help="File name to write to", default="film_calendar.ics"
+        "directory", nargs="?", help="Directory to write to", default="."
     )
     args = parser.parse_args()
 
-    seattle_films = filmcalendar.filmcalendar.FilmCalendar()
+    output_dir = Path(args.directory)
+    if output_dir.is_dir():
+        seattle_films = filmcalendar.filmcalendar.FilmCalendar()
 
-    for theater in args.theaters:
-        print(f"Scraping {theaters[theater]}")
-        theater_module = importlib.import_module(f"filmcalendar.{theater}")
-        klass = getattr(
-            theater_module, f"FilmCalendar{theaters[theater].replace(' ', '')}"
-        )
+        for theater in args.theaters:
+            print(f"Scraping {theaters[theater]}")
+            theater_module = importlib.import_module(f"filmcalendar.{theater}")
+            klass = getattr(
+                theater_module, f"FilmCalendar{theaters[theater].replace(' ', '')}"
+            )
 
-        theater_calendar = klass(calendar_name=f"{theaters[theater]} Movie Calendar")
-        theater_calendar.fetch_films()
-        theater_calendar.write(f"{theater}.ics")
-        seattle_films.append_filmcalendar(theater_calendar)
+            theater_calendar = klass(
+                calendar_name=f"{theaters[theater]} Movie Calendar"
+            )
+            theater_calendar.fetch_films()
+            theater_calendar.write(f"{args.directory}/{theater}.ics")
+            seattle_films.append_filmcalendar(theater_calendar)
 
-    seattle_films.write(args.output)
+        seattle_films.write(f"{args.directory}/film_calendar.ics")
+
+    else:
+        print(f"ERROR: {args.directory} is not a directory")
 
 
 if __name__ == "__main__":
