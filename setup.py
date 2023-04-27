c748fe21d0077d033bcb3a00fc42036d007170d5
BryantD <durrell@innocence.com>
2023-04-27 07:52:30 -0700

    Fixed missing locations for SIFF
    A bunch of theaters added for the festival -- also handled streaming movies.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="filmcalendar",
-    version="1.1.1",
+    version="1.1.2",
     description="Film calendar aggregator",
     author="Bryant Durrell",
     author_email="durrell@innocence.com",
