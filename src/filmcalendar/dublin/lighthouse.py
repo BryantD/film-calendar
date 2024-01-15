93e7fe7e8f8f4598351bccf5f317a0c77a069da3
BryantD <durrell@innocence.com>
2024-01-15 14:49:37 -0800

    Dublin: added lighthouse parsing, added a config file
    This is a first draft: it generates an ICS file but the timezones are
    probably weird. Also I need to go back and fix some basic assumptions,
    since this is pretty biased towards one timezone.
    
    Still, proof of concept! Thanks for being reasonably generic, past me.

---
+++ src/filmcalendar/dublin/lighthouse.py
@@ -0,0 +1,57 @@
+from datetime import datetime, timedelta
+
+import pytz  # Adding timezone in here quick -- this may need a refactor later
+import requests
+from bs4 import BeautifulSoup
+
+from filmcalendar import filmcalendar
+
+
+class FilmCalendarLighthouse(filmcalendar.FilmCalendar):
+    def __init__(self, **kwds):
+        super().__init__(**kwds)
+        self.address = "Market St S, Smithfield, Dublin 7, D07 R6YE, Ireland"
+        self.base_url = "https://www.lighthousecinema.ie/ajax/films-by-day-home"
+        self.timezone = pytz.timezone("Europe/Dublin")
+
+    def __str__(self):
+        return super().__str__()
+
+    def fetch_film_day(self, relative_day):
+        try:
+            req = requests.get(
+                f"{self.base_url}/{relative_day}",
+            )
+        except requests.exceptions.RequestException:
+            raise
+
+        movie_date = datetime.now() + timedelta(days=relative_day - 1)
+        movie_date.replace(hour=0, minute=0, second=0)
+
+        soup = BeautifulSoup(req.text, "html.parser")
+
+        event_data = soup.find_all("li", class_="on")
+        for film in event_data:
+            film_title = film.find("h3").find("a").get_text(strip=True)
+            film_url = film.find("h3").find("a")["href"]
+            film_duration = timedelta(
+                minutes=int(film.find("span", class_="where").get_text()[:-5])
+            )
+            film_location = f"{self.theater}: {self.address}"
+            for showing in film.find("div", class_="times").find_all("a"):
+                movie_hour, movie_minute = showing.get_text().split(":")
+                showing_date = movie_date
+                showing_date.replace(hour=int(movie_hour), minute=int(movie_minute))
+                film_date = self.timezone.localize(movie_date)
+                self.add_event(
+                    summary=film_title,
+                    dtstart=film_date,
+                    duration=film_duration,
+                    url=film_url,
+                    location=film_location,
+                )
+
+    def fetch_films(self):
+        for day in range(1, 14):
+            # 14 days is arbitrary but I think two weeks is a good window
+            self.fetch_film_day(day)
