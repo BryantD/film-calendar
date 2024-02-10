d8f67ad57e4c9d6703bcf60ec4dabffac2ec6fb1
BryantD <durrell@innocence.com>
2024-02-10 11:50:19 -0800

    Added timezone, fixed location string, removed excess function
    - added a timezone
    - fixed the location string to include the theater name
    - removed the leftover function from when I copied this film from lighthouse.py
    
    Tested in local calendar, looks good.

--- src/filmcalendar/dublin/stella.py
+++ src/filmcalendar/dublin/stella.py
@@ -3,7 +3,6 @@ import re
 from datetime import datetime, timedelta
 
 import requests
-from bs4 import BeautifulSoup
 
 from filmcalendar import filmcalendar
 
@@ -19,40 +18,6 @@ class FilmCalendarStella(filmcalendar.FilmCalendar):
 
     def __str__(self):
         return super().__str__()
-
-    def fetch_film_day(self, relative_day):
-        try:
-            req = requests.get(
-                f"{self.base_url}/{relative_day}", headers=self.req_headers
-            )
-        except requests.exceptions.RequestException:
-            raise
-
-        movie_date = datetime.now(self.timezone) + timedelta(days=relative_day)
-        movie_date = movie_date.replace(hour=0, minute=0, second=0, microsecond=0)
-
-        soup = BeautifulSoup(req.text, "html.parser")
-
-        event_data = soup.find_all("li", class_="on")
-        for film in event_data:
-            film_title = film.find("h3").find("a").get_text(strip=True)
-            film_url = film.find("h3").find("a")["href"]
-            film_duration = timedelta(
-                minutes=int(film.find("span", class_="where").get_text()[:-5])
-            )
-            film_location = f"{self.theater}: {self.address}"
-            for showing in film.find("div", class_="times").find_all("a"):
-                movie_hour, movie_minute = showing.get_text().split(":")
-                film_date = movie_date.replace(
-                    hour=int(movie_hour), minute=int(movie_minute)
-                )
-                self.add_event(
-                    summary=film_title,
-                    dtstart=film_date,
-                    duration=film_duration,
-                    url=film_url,
-                    location=film_location,
-                )
 
     def fetch_films(self):
         try:
@@ -76,20 +41,20 @@ class FilmCalendarStella(filmcalendar.FilmCalendar):
         film_duration = timedelta(minutes=120)
 
         for film in film_json:
+            base_location = film["taxonomy"][0]
             film_title = film["event_title"]
-            film_location = self.addresses[film["taxonomy"][0]]
+            film_location = (
+                f"{base_location.capitalize()}: {self.addresses[base_location]}"
+            )
             film_url = (
-                self.base_url
-                + "/events/"
-                + film["taxonomy"][0]
-                + "/"
-                + film["event_slug"]
+                self.base_url + "/events/" + base_location + "/" + film["event_slug"]
             )
             film["date"] = re.sub(r"(\d)(st|nd|rd|th)", r"\1", film["date"])
             film_date = datetime.strptime(
                 f"{film['date']} {film['time']}",
                 "%A, %B %d, %Y %I:%M %p",
             )
+            film_date = film_date.replace(tzinfo=self.timezone)
 
             self.add_event(
                 summary=film_title,
