8cb33106792da122107f28ee5d70104390076d8b
BryantD <durrell@innocence.com>
2024-11-10 17:07:02 -0800

    Update version number
    As usual I forgot to do it in the branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="filmcalendar",
-    version="1.2.1",
+    version="1.2.2",
     description="Film calendar aggregator",
     author="Bryant Durrell",
     author_email="durrell@innocence.com",
