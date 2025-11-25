2dc0372c1eac1bc4826e8499cb89b040fa401872
BryantD <durrell@innocence.com>
2025-11-25 15:03:52 -0800

    Updated for new AJAX endpoint
    Was: https://www.lighthousecinema.ie/ajax/films-by-day-home
    Is: https://www.lighthousecinema.ie/ajax/films-by-day
    
    Also had to update duration parsing logic.

--- src/filmcalendar/dublin/lighthouse.py
+++ src/filmcalendar/dublin/lighthouse.py
@@ -11,14 +11,12 @@ class FilmCalendarLightHouse(filmcalendar.FilmCalendar):
     def __init__(self, **kwds):
         super().__init__(**kwds)
         self.address = "Market St S, Smithfield, Dublin 7, D07 R6YE"
-        self.base_url = "https://www.lighthousecinema.ie/ajax/films-by-day-home"
+        self.base_url = "https://www.lighthousecinema.ie/ajax/films-by-day"
 
     def __str__(self):
         return super().__str__()
 
     def fetch_film_day(self, relative_day):
-        # Added to debug weird Light House issue
-        logging.basicConfig(format="%(message)s")
         log = logging.getLogger(__name__)
 
         try:
@@ -37,17 +35,35 @@ class FilmCalendarLightHouse(filmcalendar.FilmCalendar):
         for film in event_data:
             film_title = film.find("h3").find("a").get_text(strip=True)
             film_url = film.find("h3").find("a")["href"]
-            try:
-                film.find("span", class_="where").get_text()
-            except AttributeError:
-                log.warning(f"Skipping {film_title} because it has no time")
-                log.warning(film.prettify())
-            film_duration = timedelta(
-                minutes=int(film.find("span", class_="where").get_text()[:-5])
-            )
+            # Try to find duration
+            duration_text = ""
+            duration_elem = film.find("div", class_="shortened-aside")
+            if duration_elem:
+                duration_text = duration_elem.get_text(strip=True)
+                # Format is usually "Rating / X Mins"
+                if "Mins" in duration_text:
+                    try:
+                        minutes = int(
+                            duration_text.split("/")[-1].replace("Mins", "").strip()
+                        )
+                        film_duration = timedelta(minutes=minutes)
+                    except ValueError:
+                        log.warning(
+                            f"Could not parse duration from '{duration_text}' for {film_title}"  # noqa: E501
+                        )
+                        continue
+                else:
+                    log.warning(
+                        f"Could not find 'Mins' in '{duration_text}' for {film_title}"
+                    )
+                    continue
+            else:
+                continue
             film_location = f"{self.theater}: {self.address}"
             for showing in film.find("div", class_="times").find_all("a"):
                 movie_hour, movie_minute = showing.get_text().split(":")
+                # Clean up "Sold out" or other text
+                movie_minute = "".join(filter(str.isdigit, movie_minute))
                 film_date = movie_date.replace(
                     hour=int(movie_hour), minute=int(movie_minute)
                 )
